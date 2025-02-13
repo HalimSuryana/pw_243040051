106d3635575f24c75ed0ae8b088435ef319cbd71
HalimSuryana <halimsuryana23@gmail.com>
2025-02-13 10:32:25 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Halim Suryana";
+$email = "halimsuryana23@gmail.com";
+$jurusan = "Teknik informatika";
+$universitas = "universitas pasundan";
+$hobi = "Bermain game";
+    echo "<h1>$nama</h1>";
+    echo "<h1>$email</h1>";
+    echo "<h1>$jurusan</h1>";
+    echo "<h1>$universitas</h1>";
+    echo "<h1>$hobi</h1>";
+    echo "<p>Halo,saya $nama dan saya $jurusan di $universitas </p>";
+?>
